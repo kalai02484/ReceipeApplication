72ebfe93a20fb923599a6e469042ccf1accf214c
kalai02484 <kalai02484@gmail.com>
2026-01-28 09:26:54 +0530

    Router Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Error from './pages/Error'
+import ReceipeDetails from './pages/ReceipeDetails'
+import Favorites from './pages/Favorites'
 
 const App = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favorites' element={<Favorites />} />
+        <Route path='/receipe/:id' element={<ReceipeDetails />} />
+        <Route path='*' element = {<Error />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/pages/ReceipeDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReceipeDetails = () => {
+  return (
+    <div>
+      ReceipeDetails Page
+    </div>
+  )
+}
+
+export default ReceipeDetails
